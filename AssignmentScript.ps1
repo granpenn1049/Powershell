aa9054d51c22ea0eafaedc2eca9a23634e121db2
granpenn1049 <grantapenner@gmail.com>
2024-07-16 22:04:17 -0400

    Able to separate data into two arrays (one numeric, one alphabetic). Need to work on adding scientific notation to $IntArray.

--- AssignmentScript.ps1
+++ AssignmentScript.ps1
@@ -1,5 +1,14 @@
-$FileContent = Get-Content "C:\Users\penne\OneDrive\Documents\Powershell\PowershellAssignment\Samples\Sample2.txt"
+#Arrays
+$FileContent = Get-Content "C:\Users\penne\OneDrive\Documents\Powershell\PowershellAssignment\Samples\Sample3.txt"
 $ContentArray = $FileContent.split(",")
+$IntArray = $ContentArray -match "^[\d\.+\d]+$"
+$StringArrary = $ContentArray -match "^[A-Z]+$"
 
-$IntArray = Return $ContentArray -match "^[\d\.]+$"
-Write-Host $IntArray
+$IntArray
+$StringArrary
+
+$IntDataType = $IntArray | ForEach-Object {$_.GetType()}
+$IntDataType
+
+$StringDataType = $StringArrary | ForEach-Object {$_.GetType()}
+$StringDataType
