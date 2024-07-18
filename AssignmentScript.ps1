1a8639269561c2faf261440392583f9edc05dc1d
granpenn1049 <grantapenner@gmail.com>
2024-07-17 21:31:08 -0400

    Array successfully sorted based on passed parameters

--- AssignmentScript.ps1
+++ AssignmentScript.ps1
@@ -1,14 +1,40 @@
 #Arrays
-$FileContent = Get-Content "C:\Users\penne\OneDrive\Documents\Powershell\PowershellAssignment\Samples\Sample3.txt"
+param (
+    [String]$Filepath,
+    [String]$SortOrder = "Ascending" -or "Descending",
+    [String]$DataType = "Numeric" -or "Alpha" -or "Both"
+)
+
+$FileContent = Get-Content "$Filepath"
 $ContentArray = $FileContent.split(",")
 $IntArray = $ContentArray -match "^[\d\.+\d]+$"
 $StringArrary = $ContentArray -match "^[A-Z]+$"
 
-$IntArray
-$StringArrary
+#Numeric, Alpha, or Both -Ascending
+if(($DataType -eq "Numeric") -and ($SortOrder -eq "Ascending")) {
+    $IntArray | Sort-Object
+    break
+}
+elseif(($DataType -eq "Alpha") -and ($SortOrder -eq "Ascending")) {
+    $StringArray | Sort-Object
+}
+elseif(($DataType -eq "Both") -and ($SortOrder -eq "Ascending")) {
+    $IntArray+$StringArrary | Sort-Object
+}
+else {
+    Write-Error 'Must pass in $Filepath, $SortOrder ("Ascending" or "Descending"), and $DataType ("Numeric", "Alpha", or "Both")'
+}
 
-$IntDataType = $IntArray | ForEach-Object {$_.GetType()}
-$IntDataType
-
-$StringDataType = $StringArrary | ForEach-Object {$_.GetType()}
-$StringDataType
+#Numeric, Alpha, or Both -Descending
+if(($DataType -eq "Numeric") -and ($SortOrder -eq "Descending")) {
+    $IntArray | Sort-Object -Descending
+}
+elseif(($DataType -eq "Alpha") -and ($SortOrder -eq "Descending")) {
+    $StringArray | Sort-Object -Descending
+}
+elseif(($DataType -eq "Both") -and ($SortOrder -eq "Descending")) {
+    $IntArray+$StringArrary | Sort-Object -Descending
+}
+else {
+    Write-Error 'Must pass in $Filepath, $SortOrder ("Ascending" or "Descending"), and $DataType ("Numeric", "Alpha", or "Both")'
+}
