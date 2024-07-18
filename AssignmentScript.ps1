5b9fe82db6dd0efea1e14c892ef993b9a4371236
granpenn1049 <grantapenner@gmail.com>
2024-07-17 21:43:32 -0400

    Formatted script

--- AssignmentScript.ps1
+++ AssignmentScript.ps1
@@ -12,29 +12,29 @@ $NumericArray = $ContentArray -match "^[\d\.+\d]+$"
 $AlphaArray = $ContentArray -match "^[A-Z]+$"
 
 #Numeric, Alpha, or Both -Ascending
-if(($DataType -eq "Numeric") -and ($SortOrder -eq "Ascending")) {
+if (($DataType -eq "Numeric") -and ($SortOrder -eq "Ascending")) {
     $NumericArray | Sort-Object
     break
 }
-elseif(($DataType -eq "Alpha") -and ($SortOrder -eq "Ascending")) {
+elseif (($DataType -eq "Alpha") -and ($SortOrder -eq "Ascending")) {
     $AlphaArray | Sort-Object
 }
-elseif(($DataType -eq "Both") -and ($SortOrder -eq "Ascending")) {
-    $NumericArray+$AlphaArray | Sort-Object
+elseif (($DataType -eq "Both") -and ($SortOrder -eq "Ascending")) {
+    $NumericArray + $AlphaArray | Sort-Object
 }
 else {
     Write-Error 'Must pass in $Filepath, $SortOrder ("Ascending" or "Descending"), and $DataType ("Numeric", "Alpha", or "Both")'
 }
 
 #Numeric, Alpha, or Both -Descending
-if(($DataType -eq "Numeric") -and ($SortOrder -eq "Descending")) {
+if (($DataType -eq "Numeric") -and ($SortOrder -eq "Descending")) {
     $NumericArray | Sort-Object -Descending
 }
-elseif(($DataType -eq "Alpha") -and ($SortOrder -eq "Descending")) {
+elseif (($DataType -eq "Alpha") -and ($SortOrder -eq "Descending")) {
     $AlphaArray | Sort-Object -Descending
 }
-elseif(($DataType -eq "Both") -and ($SortOrder -eq "Descending")) {
-    $NumericArray+$AlphaArray | Sort-Object -Descending
+elseif (($DataType -eq "Both") -and ($SortOrder -eq "Descending")) {
+    $NumericArray + $AlphaArray | Sort-Object -Descending
 }
 else {
     Write-Error 'Must pass in $Filepath, $SortOrder ("Ascending" or "Descending"), and $DataType ("Numeric", "Alpha", or "Both")'
